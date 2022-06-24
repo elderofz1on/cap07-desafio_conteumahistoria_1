325338e4a40d1905c0a1415e822f2826d8f48cc2
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 16:55:44 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+// Equivale a um botão para reset, para reiniciar uma nova história.
+input.onButtonPressed(Button.AB, function () {
+	
+})
+let Categoria = 0
+let QtdeColunasJaDefinidas = 1
+let QtdeColunasAUsar = 6
